e9d6c297737c464434867c256cd361df31d5e1b1
Anthony <antilori25@gmail.com>
2026-07-03 19:22:14 +0200

    dotenv file added

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const path = require("path");
 const { MongoClient, ObjectId } = require("mongodb");
 const nodemailer = require("nodemailer");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
